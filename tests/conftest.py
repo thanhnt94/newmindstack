b2e5db76332c8b8c546931700875a668002bc5b3
thanhnt94 <thanhnt94@gmail.com>
2025-09-26 23:57:44 +0700

    Handle DuckDuckGo throttling with retries

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,31 @@ import os
 import sys
 
 import pytest
+import types
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+if "duckduckgo_search" not in sys.modules:
+    duckduckgo_search = types.ModuleType("duckduckgo_search")
+
+    class _StubDuckDuckGoSearchException(Exception):
+        pass
+
+    class _StubDDGS:
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc, tb):
+            return False
+
+        def images(self, *_args, **_kwargs):
+            return []
+
+    duckduckgo_search.DDGS = _StubDDGS
+    exceptions_module = types.ModuleType("duckduckgo_search.exceptions")
+    exceptions_module.DuckDuckGoSearchException = _StubDuckDuckGoSearchException
+    sys.modules["duckduckgo_search"] = duckduckgo_search
+    sys.modules["duckduckgo_search.exceptions"] = exceptions_module
 
 from mindstack_app import create_app, db
 from mindstack_app.config import Config
